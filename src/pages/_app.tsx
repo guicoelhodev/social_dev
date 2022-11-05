582294ab2053d60de844d7757d645a6ad4779b1f
guicoelhodev <gs.coelho@outlook.com>
2022-11-05 17:52:23 -0300

    feat: remove business logic from app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,12 @@
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { ThemeProvider } from "styled-components";
+import { GlobalProvider } from "../context";
 import { GlobalStyles } from "../styles/globalStyle";
-import { darkTheme, lightTheme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [theme, setTheme] = useState("dark");
-
-  const themeToggler = () => {
-    return theme === "light" ? setTheme("dark") : setTheme("light");
-  };
-
-  useEffect(() => {
-    console.log(theme);
-  }, [theme]);
-
   return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+    <GlobalProvider>
       <GlobalStyles />
-      <button onClick={() => themeToggler()}>Switch Theme</button>
       <Component {...pageProps} />;
-    </ThemeProvider>
+    </GlobalProvider>
   );
 }
