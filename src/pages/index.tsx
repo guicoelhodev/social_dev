8ca29cc8e0aeaf1235899faf8f43944f4e9d616c
guicoelhodev <gs.coelho@outlook.com>
2022-11-05 17:52:44 -0300

    feat: add content to test current mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,1 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Social Dev | Advice</title>
-      </Head>
-      <p>Hello world</p>
-      <h1>This is a simple h1</h1>
-      <h2>This is a simple h2</h2>
-    </>
-  );
-}
+export { default } from "../content/Home";
