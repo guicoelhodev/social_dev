df146fd758e321cf1404d6b89cee78bb737d501e
guicoelhodev <gs.coelho@outlook.com>
2022-11-05 19:19:15 -0300

    feat: add config to receive external images from media.tenor domain

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["media.tenor.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
