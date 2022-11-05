1595c701402e38c7e7004739b5e6f83020ca909f
guicoelhodev <gs.coelho@outlook.com>
2022-11-05 17:51:40 -0300

    feat: create globalContext to add allContext childrens

---
+++ src/context/index.tsx
@@ -0,0 +1,13 @@
+import { FC, ReactNode } from "react";
+import { ThemeContextProvider } from "./theme";
+
+interface IGlobalContext {
+  children: ReactNode;
+}
+export const GlobalProvider: FC<IGlobalContext> = ({ children }) => {
+  return (
+    <>
+      <ThemeContextProvider>{children}</ThemeContextProvider>
+    </>
+  );
+};
