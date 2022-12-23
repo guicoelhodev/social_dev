74978858c32e738d3a7f1a5a8120857e3ee2717c
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 17:04:31 -0300

    test: create testing to verify TextInput component

--- src/components/UI/inputs/TextInput/__test__/index.spec.tsx
+++ src/components/UI/inputs/TextInput/__test__/index.spec.tsx
@@ -1,11 +1,52 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, test } from 'vitest';
 import { TextInput } from '@components/UI/inputs';
 
 describe('Should test TextInput component', () => {
   test('Should render TextInput', () => {
-    const { debug } = render(<TextInput label="Name" />);
+    render(
+      <TextInput
+        label="Name"
+        register={{
+          value: 'aaaaa',
+        }}
+      />
+    );
 
-    console.log(debug);
+    // screen.logTestingPlaygroundURL();
+    const input = screen.getByRole(/textbox/i);
+
+    expect(input).toBeInTheDocument();
+  });
+
+  test('Should render TextInput with input value', () => {
+    render(
+      <TextInput
+        label="Name"
+        register={{
+          value: 'myEmail@email.com',
+        }}
+      />
+    );
+
+    const input = screen.getByRole(/textbox/i);
+
+    expect(input).toHaveValue('myEmail@email.com');
+  });
+
+  test('Should render label content', () => {
+    render(
+      <TextInput
+        label="Select your name"
+        register={{
+          value: 'myEmail@email.com',
+        }}
+      />
+    );
+
+    screen.logTestingPlaygroundURL();
+    const label = screen.getByText(/select your name/i);
+
+    expect(label).toHaveTextContent('Select your name');
   });
 });
