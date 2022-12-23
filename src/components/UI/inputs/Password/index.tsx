c529a8a5f76f7f59ff696466145c9bb2b086839b
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 15:52:48 -0300

    feat: create UI password input component

---
+++ src/components/UI/inputs/Password/index.tsx
@@ -0,0 +1,40 @@
+import React, {
+  ChangeEvent,
+  useId,
+  RefObject,
+  forwardRef,
+  FC,
+  useState,
+} from 'react';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import * as S from './style';
+
+interface ITextInput {
+  label: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  id?: string;
+  placeholder?: string;
+  defaultValue?: string;
+  error?: string;
+}
+
+export const PasswordInput: FC<ITextInput> = (props) => {
+  const [viewPassword, setViewPassword] = useState(false);
+  const inputId = useId();
+  return (
+    <S.Container>
+      <label htmlFor={inputId}>{props.label}</label>
+      <input
+        {...props}
+        type={viewPassword ? 'text' : 'password'}
+        id={props.id ? props.id : inputId}
+      />
+      <S.ViewPassword onClick={() => setViewPassword((prev) => !prev)}>
+        {viewPassword ? <AiFillEye /> : <AiFillEyeInvisible />}
+      </S.ViewPassword>
+
+      <div>{props.error}</div>
+    </S.Container>
+  );
+};
