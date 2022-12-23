951cf70b2d1e6b440b3d87e6ec2c516a5a86b876
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 18:45:50 -0300

    feat: add directory file path

--- src/content/Login/index.tsx
+++ src/content/Login/index.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from 'next';
 import { Background } from '@pages/Login/Background';
 import { SignIn } from '@pages/Login/SignIn';
+import { useSession } from 'next-auth/react';
+
 import * as S from './style';
 
 const LoginPage: NextPage = () => {
+  const { data } = useSession();
+
+  console.log(data);
+
   return (
     <S.Container>
       {/* <Background /> */}
