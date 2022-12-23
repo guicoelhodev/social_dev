e0f85a7b42d3e847c3c7666e863af5c6a2860f7a
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 16:12:07 -0300

    feat: add test to use into project with vitest

--- src/content/Login/style.ts
+++ src/content/Login/style.ts
@@ -2,20 +2,21 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  max-width: 81.25rem;
   height: 100vh;
-
+  margin: 0 auto;
   display: grid;
   grid-template-columns: 1fr minmax(auto, 22rem);
   padding: 2rem;
 
-  @media (max-width:820px){
+  @media (max-width: 820px) {
     grid-template-columns: 1fr;
 
-    section:nth-child(1){
+    section:nth-child(1) {
       display: none;
     }
-    
-     section:nth-child(2){
+
+    section:nth-child(2) {
       margin: 0 auto;
       max-width: 500px;
       width: 100%;
