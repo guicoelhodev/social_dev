c1988c687b9d47ed5b16152232d4c1c951e04722
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 19:07:41 -0300

    feat: add Auth logic to redirect user

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,28 @@ import type { AppProps } from 'next/app';
 import { GlobalProvider } from '../context';
 import { SessionProvider } from 'next-auth/react';
 import { GlobalStyles } from '@style/globalStyle';
+import { AuthComponent } from 'src/global/auth/AuthComponent';
 
+interface IAuthProps extends AppProps {
+  Component: any;
+}
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
-}: AppProps) {
+}: IAuthProps) {
   return (
     <GlobalProvider>
       <SessionProvider session={session}>
         <GlobalStyles />
         <Component {...pageProps} />
+
+        {Component.auth ? (
+          <AuthComponent>
+            <Component {...pageProps} />
+          </AuthComponent>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </SessionProvider>
     </GlobalProvider>
   );
