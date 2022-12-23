dc04e1c34a85763714a8d690b45ab11759d83159
guicoelhodev <gs.coelho@outlook.com>
2022-12-23 18:44:44 -0300

    feat: create nextAuth config file

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,31 @@
+import { NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+
+import GithubProvider from 'next-auth/providers/github';
+import LinkedInProvider from 'next-auth/providers/linkedin';
+
+const authOptions: NextAuthOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID!,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    // signIn: '/login',
+  },
+  callbacks: {
+    async signIn({ user }) {
+      console.log('user', user);
+
+      return true;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
