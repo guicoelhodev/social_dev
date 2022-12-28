49f54b4fcfa8e4c7cadf5c5d44f2f3a5c943a2de
guicoelhodev <gs.coelho@outlook.com>
2022-12-28 19:10:58 -0300

    feat: create subComponent to show all languages avaliables that user should choose to add in your profile

---
+++ src/content/Profile/ModalLanguages/languages.ts
@@ -0,0 +1,102 @@
+import {
+  DiAngularSimple,
+  DiDocker,
+  DiJava,
+  DiJavascript1,
+  DiRasberryPi,
+  DiReact,
+  DiRuby,
+  DiRust,
+  DiSwift,
+} from 'react-icons/di';
+import { SiSvelte } from 'react-icons/si';
+import {
+  FaCss3Alt,
+  FaFlask,
+  FaLinux,
+  FaNode,
+  FaPhp,
+  FaPython,
+  FaRProject,
+  FaSass,
+  FaUnity,
+} from 'react-icons/fa';
+
+export const languages = [
+  {
+    name: 'Javascript',
+    icon: DiJavascript1,
+  },
+  {
+    name: 'Java',
+    icon: DiJava,
+  },
+  {
+    name: 'Docker',
+    icon: DiDocker,
+  },
+  {
+    name: 'React',
+    icon: DiReact,
+  },
+  {
+    name: 'Angular',
+    icon: DiAngularSimple,
+  },
+  {
+    name: 'Ruby',
+    icon: DiRuby,
+  },
+  {
+    name: 'Rust',
+    icon: DiRust,
+  },
+  {
+    name: 'Python',
+    icon: FaPython,
+  },
+  {
+    name: 'RasberryPi',
+    icon: DiRasberryPi,
+  },
+  {
+    name: 'Swift',
+    icon: DiSwift,
+  },
+  {
+    name: 'Svelte',
+    icon: SiSvelte,
+  },
+  {
+    name: 'CSS 3',
+    icon: FaCss3Alt,
+  },
+  {
+    name: 'Linux',
+    icon: FaLinux,
+  },
+  {
+    name: 'Node',
+    icon: FaNode,
+  },
+  {
+    name: 'Php',
+    icon: FaPhp,
+  },
+  {
+    name: 'R',
+    icon: FaRProject,
+  },
+  {
+    name: 'Sass',
+    icon: FaSass,
+  },
+  {
+    name: 'Unity',
+    icon: FaUnity,
+  },
+  {
+    name: 'Jest',
+    icon: FaFlask,
+  },
+];
