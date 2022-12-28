50ef4231642f8130ef4cbde91ba7537bbe892dfd
guicoelhodev <gs.coelho@outlook.com>
2022-12-28 19:07:23 -0300

    refactor: add Layout into Home page

--- src/content/Home/style.ts
+++ src/content/Home/style.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100%;
-  min-height: 100vh;
+  min-height: calc(100vh - 5rem);
   width: 100%;
   padding: 1rem;
 
