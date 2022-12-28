36aa9fe2e6b4ef37c3f338aad4c6bee8798f47f5
guicoelhodev <gs.coelho@outlook.com>
2022-12-28 19:14:03 -0300

    build: add normalize.css to reset all css browsers default

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -52,6 +52,11 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Oxanium:wght@400;600&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          ></link>
         </Head>
         <body>
           <Main />
