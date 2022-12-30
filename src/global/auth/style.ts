faed020c95ea5522438d81a60989b8422ccc9b36
guicoelhodev <gs.coelho@outlook.com>
2022-12-30 20:14:06 -0300

    fix: try to fix build error

--- src/global/auth/style.ts
+++ src/global/auth/style.ts
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Loading = styled.div`
   color: ${(props) => props.theme.primaryColor};
   width: 100vw;
-  height: ${(props) => props.theme.maxHeight};
+  //height: ${(props) => props.theme.maxHeight};
+  height: 100vh;
 
   display: grid;
   place-items: center;
