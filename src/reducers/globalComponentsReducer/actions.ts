a91b3a19a0c1eb45b3a7efe8d05a4943669e805d
guicoelhodev <gs.coelho@outlook.com>
2023-01-02 19:38:22 -0300

    feat: add new acitons to handle languages state and change action function

--- src/reducers/globalComponentsReducer/actions.ts
+++ src/reducers/globalComponentsReducer/actions.ts
@@ -2,16 +2,21 @@ export enum ActionsGlobalComponents {
   HANDLE_CHAT_STATE = 'HANDLE_CHAT_STATE',
   HANDLE_CREATE_POST = 'HANDLE_CREATE_POST',
   HANDLE_ADD_FRIEND = 'HANDLE_ADD_FRIEND',
+  HANDLE_LANGUAGES_STATE = 'HANDLE_LANGUAGES',
 }
 
-export const handleChatState = () => {
+export const handleChatStateActions = () => {
   return { type: ActionsGlobalComponents.HANDLE_CHAT_STATE };
 };
 
-export const handleCreatPostState = () => {
+export const handleCreatPostStateActions = () => {
   return { type: ActionsGlobalComponents.HANDLE_CREATE_POST };
 };
 
-export const handleAddFriend = () => {
+export const handleAddFriendActions = () => {
   return { type: ActionsGlobalComponents.HANDLE_ADD_FRIEND };
+};
+
+export const handleUserLanguagesActions = () => {
+  return { type: ActionsGlobalComponents.HANDLE_LANGUAGES_STATE };
 };
