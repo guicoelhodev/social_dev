20206934982e578a9e89fe9353d638af6c11d454
guicoelhodev <gs.coelho@outlook.com>
2023-01-03 23:33:35 -0300

    feat: add into SignIn callback verifyUser rule function

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import { verifyCredentials } from '@http/login/POST/verifyCredentials';
 import { NextAuthOptions } from 'next-auth';
 import NextAuth from 'next-auth/next';
 
@@ -14,10 +15,42 @@ const authOptions: NextAuthOptions = {
       clientId: process.env.LINKEDIN_CLIENT_ID!,
       clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
     }),
+    // {
+    //   async au(authorize) {
+
+    //     return Promise.resolve(authorize);
+    //   },
+    // },
   ],
   pages: {
     signIn: '/login',
     error: '/login',
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const socialAuth = ['github', 'linkedin'];
+      //console.log('signData', signData);
+      const params = {
+        email: user.email!,
+        password: '',
+        external: socialAuth.includes(account?.provider!),
+      };
+      // const params = {
+      //   email: 'sammonteiro10@gmail.com',
+      //   password: 'jojo',
+      //   external: false,
+      // };
+      const response = await verifyCredentials(params);
+      // credentials = response as any;
+      console.log('aaaaaaaaaaaaaaaaaaa', profile);
+
+      // sessionStorage.setItem('@USER_LOGIN', JSON.stringify(response));
+      return true;
+    },
+    async session(p) {
+      console.log('p', p);
+      return p.session;
+    },
   },
   secret: process.env.NEXTAUTH_SECRET,
 };
