72bbe4ec6198b5dd1257dcf01b6d42b3e0e7ffff
guicoelhodev <gs.coelho@outlook.com>
2023-01-21 22:47:29 -0300

    feat: add TabNewPost into homePage

--- src/content/Home/index.tsx
+++ src/content/Home/index.tsx
@@ -3,15 +3,24 @@ import { Chat } from '@components/FC/Chat';
 import { ModalLanguages } from '@components/FC/ModalLanguages';
 import { Layout } from '@components/UI/Layout';
 import { UserActionsContext } from '@context/userActions';
-import React, { Suspense, useContext } from 'react';
+import React, { useContext } from 'react';
 import { BasicUserInfo } from './BasicUserInfo';
-import { InternalUserArticle } from './InternalUserArticle';
+import { InternalUserArticle } from '../../components/FC/InternalUserArticle';
+import { Search } from './Search';
 import * as S from './style';
+import { useAppInteractionStore } from 'src/stories/appInteraction';
+import { useTabListPosts } from '@services/http/tabNews/GET/listPosts';
+import { TabNewsPost } from '@components/FC/TabNewsPost';
 
 const arr = [false, true, false, false, true, true, true, true];
 
 const Home: NextPageAuthenticated = () => {
   const { globalComponentsState } = useContext(UserActionsContext);
+
+  const search = useAppInteractionStore((state) => state.search);
+  const searchType = useAppInteractionStore((state) => state.searchType);
+
+  const { data: tabnewsList } = useTabListPosts({});
 
   const handleLanguages = (langs: string[]) => console.log(langs);
   return (
@@ -26,11 +35,22 @@ const Home: NextPageAuthenticated = () => {
           <BasicUserInfo />
         </div>
         <S.PageContent>
-          <S.ListInternalArticles>
-            {arr.map((item) => (
-              <InternalUserArticle shared={item} />
-            ))}
-          </S.ListInternalArticles>
+          <Search />
+          {search === 'feed' && (
+            <S.ListArticles>
+              {arr.map((item) => (
+                <InternalUserArticle shared={item} />
+              ))}
+            </S.ListArticles>
+          )}
+
+          {search === 'search' && searchType === 'tabNews' && (
+            <S.ListArticles>
+              {tabnewsList?.map((post) => (
+                <TabNewsPost slug={post.slug} user={post.owner_username} />
+              ))}
+            </S.ListArticles>
+          )}
         </S.PageContent>
       </S.Container>
     </Layout>
